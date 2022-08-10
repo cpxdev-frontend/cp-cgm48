e7b9b347bd08a3cad48e40dda42f3d457fde0687
Chinnathorn Promnaruritr <56143786+cpx2017@users.noreply.github.com>
2022-08-10 15:11:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -253,7 +253,7 @@ function App() {
                           color="secondary"
                         />
                       }
-                      label="Reduce Graphic"
+                      label="Focus on Efficiency"
                     />
                       </div>
                     )}

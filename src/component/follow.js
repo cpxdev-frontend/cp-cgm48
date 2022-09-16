1c9d276df3af7d967e0542d32c3ac0abbf9de0a3
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2022-09-16 08:52:36 +0700

    Text edit

--- src/component/follow.js
+++ src/component/follow.js
@@ -71,22 +71,22 @@ const Contact = ({fet, setSec}) => {
                 <Typography color="textSecondary">
                 You have many way to follow every update from Social Platform
                 <ListItem>
-                    <ListItemText primary="Facebook" secondary={(<a href='//www.facebook.com/cgm48official' target='_blank'>bnk48official</a>)} />
+                    <ListItemText primary="Facebook" secondary={(<a href='//www.facebook.com/cgm48official' target='_blank'>cgm48official</a>)} />
                   </ListItem>
                   <ListItem>
-                    <ListItemText primary="Instagram" secondary={(<a href='//www.instagram.com/cgm48official' target='_blank'>bnk48</a>)} />
+                    <ListItemText primary="Instagram" secondary={(<a href='//www.instagram.com/cgm48official' target='_blank'>cgm48official</a>)} />
                   </ListItem>
                   <ListItem>
                     <ListItemText primary="Mobile Application" secondary={(<a href='//app.bnk48.com' target='_blank'>iAM48 Mobile Application. Avaliable to download in Apple App Store or Google Play Store</a>)} />
                   </ListItem>
                   <ListItem>
-                    <ListItemText primary="Twitter" secondary={(<a href='//twitter.com/cgm48official' target='_blank'>bnk48official</a>)} />
+                    <ListItemText primary="Twitter" secondary={(<a href='//twitter.com/cgm48official' target='_blank'>cgm48official</a>)} />
                   </ListItem>
                   <ListItem>
-                    <ListItemText primary="Tiktok" secondary={(<a href='//www.tiktok.com/@cgm4848official_th' target='_blank'>@bnk48official_th</a>)} />
+                    <ListItemText primary="Tiktok" secondary={(<a href='//www.tiktok.com/@cgm4848official_th' target='_blank'>@cgm48official_th</a>)} />
                   </ListItem>
                   <ListItem>
-                    <ListItemText primary="Youtube" secondary={(<a href='//www.youtube.com/c/BNK48OfficialYouTubeChannel' target='_blank'>BNK48OfficialYouTubeChannel</a>)} />
+                    <ListItemText primary="Youtube" secondary={(<a href='//www.youtube.com/c/CGM48Official' target='_blank'>CGM48Official</a>)} />
                   </ListItem>
                   <ListItem>
                     <ListItemText primary="Also Streaming Platform" secondary={(<a href='/music' target='_blank'>Go to Music page</a>)} />

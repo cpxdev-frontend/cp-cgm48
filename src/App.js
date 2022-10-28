bfac4483335282ecb7df7c6bd0b5d740059fc9fd
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2022-10-28 08:36:44 +0700

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -844,7 +844,9 @@ transitionDuration={500}
             </p>
             )}
 
-              <a onClick={() => item.link.includes('https://') ? window.open(item.link, '_blank') : {History.push(item.link); setpopup(false)}} className='mt-1'>
+              <a onClick={() => {
+                if (item.link.includes('https://')) { window.open(item.link, '_blank') } else {History.push(item.link); setpopup(false);
+              }}} className='mt-1'>
                   Reference Link
               </a>
             <br />
@@ -889,7 +891,9 @@ transitionDuration={500}
                 This event has been started in <b>{moment.unix(newspop[0].timerange[0]).format('ddd DD MMMM yyyy H:mm A')}</b> to <b>{moment.unix(newspop[0].timerange[1]).format('ddd DD MMMM yyyy H:mm A')}</b>
             </p>
             )}
-            <a onClick={() => newspop[0].link.includes('https://') ? window.open(newspop[0].link, '_blank') : {History.push(newspop[0].link); setpopup(false)}} className='mt-1'>
+            <a onClick={() => {
+                if (newspop[0].link.includes('https://')) { window.open(newspop[0].link, '_blank') } else {History.push(newspop[0].link); setpopup(false);
+              }}} className='mt-1'>
                 Reference Link
             </a>
             <br />

fb7a0d8b00ce43d3e693756a483931260a9dfe02
Chinnathorn P <chinnathorn.p@gmail.com>
2022-10-30 16:35:12 +0700

    Update

--- src/component/requesthour.js
+++ src/component/requesthour.js
@@ -1,60 +1,34 @@
 import { CardContent, Card, Typography, Grow, Fade } from '@material-ui/core';
+import { CardContent, Card, Typography, Grow, Fade, CardActionArea } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import React from 'react'
 
 const RequestHr = ({fet, setSec}) => {
+  const [arr, setArr] = React.useState(null)
+
     React.useEffect(() =>{
         setSec('Request Hour 2022')
+        fetch(fet + '/cgm48/requesthrlist', {
+          method :'post'
+      })
+          .then(response => response.json())
+          .then(data => {
+            if (data.status == true) {
+              setArr(data.res)
+            } else {
+              setArr([])
+            }
+          }).catch(() => {
+            setArr([])
+          })
     },[])
     return ( 
         <>
-           {window.innerWidth > 1200 && (
-              <div class="video-background">
-               <Fade in={true} timeout={800}>
-                <img src="https://cdn.jsdelivr.net/gh/cpx2017/cpxcdnbucket@latest/bnk48/mainoff.jpg"  width={window.innerWidth} />
-                  </Fade>
-          </div>
-            )}
-                {window.innerWidth >1200 ? (
-                <div className="cover mt-4">
-                <Grow in={true} timeout={1000}>
-              <Card className="col-md-4 m-5">
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                BNK48 and CGM48 Request Hour 2022
-                </Typography>
-                <hr />
-                <Typography color="textSecondary">
-                The first concert of BNK48 and CGM48 that you can participate in voting <b>25 songs</b> from <b>124 songs</b> of BNK48 and CGM48 songs for this concert. 
-                </Typography>
-                <hr />
-                <Typography variant="body1" component="p">
-                 You can check voting summary and virtualization chart result below.
-                </Typography>
-              </CardContent>
-                </Card>
-                </Grow>
-              </div>
-              ) : (
-            <div className="bnktheme pb-5 pt-2">
-        <Grow in={true} timeout={1000}>
-      <Card className="ml-2 mr-2">
-      <CardContent>
-                <Typography variant="h5" component="h2">
-                BNK48 and CGM48 Request Hour 2022
-                </Typography>
-                <hr />
-                <Typography color="textSecondary">
-                The first concert of BNK48 and CGM48 that you can participate in voting <b>25 songs</b> from <b>124 songs</b> of BNK48 and CGM48 songs for this concert. 
-                </Typography>
-                <hr />
-                <Typography variant="body1" component="p">
-                 You can check voting summary and virtualization chart result below.
-                </Typography>
-              </CardContent>
-        </Card>
-        </Grow>
-      </div>
-          )}
 
 
         <div className={'stage text-center ' + (window.innerWidth > 700 ? 'p-5' : 'p-2')}>
@@ -63,9 +37,51 @@ const RequestHr = ({fet, setSec}) => {
                     Voting is end. The voting are being collected. Please come back again in November 1, 2022
                 </CardContent>
             </Card>
+
+        <TableContainer>
+                <Table stickyHeader aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Rank</TableCell>
+                      <TableCell align="center">Member Image</TableCell>
+                      <TableCell align="center">Song Name</TableCell>
+                      <TableCell align="center">Song Artist</TableCell>
+                      <TableCell align="right">Spent Token</TableCell>
+                    </TableRow>
+                  </TableHead>
+          {
+            arr == null ? (
+              <TableBody>
+              <TableCell colSpan={5} align='center'><img src="https://cdn.jsdelivr.net/gh/cpx2017/cpxcdnbucket@main/main/bnk-circular.svg" width="40px" /></TableCell>
+        </TableBody>
+            ) : ((arr.length == 0) ? (
+              <TableBody>
+                  <TableCell colSpan={5} align='center'>Voting is end. The voting are being collected. Please come back again in November 1, 2022</TableCell>
+            </TableBody>
+            ) : arr.map((item) => (
+                <TableBody key={item.trackID} className='text-left' onClick={() => window.open(item.reflink,'_blank')}>
+                        <TableCell component="th">
+                          {item.rank}
+                        </TableCell>
+                        <TableCell align="center">
+                            <img src={item.src} width={100} />
+                          </TableCell>
+                          <TableCell align="center">
+                          {item.songName}
+                          </TableCell>
+                          <TableCell align="center">
+                          {item.songArtist}
+                          </TableCell>
+                          <TableCell align="right">
+                          {item.spentToken}
+                          </TableCell>
+                  </TableBody>
+            )))
+          }
+            </Table>
+              </TableContainer>
         </div>
         </>
      );
-}
- 
-export default RequestHr;
+        }
+        export default RequestHr

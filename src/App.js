9093f754b9a06ba930cd0d8324c9eefa6b87e8b2
Chinnathorn P <chinnathorn.p@gmail.com>
2022-12-31 21:59:56 +0700

    unix

--- src/App.js
+++ src/App.js
@@ -308,7 +308,7 @@ function App() {
               })
             }
             }
-            const withprio = data.filter(x => x.priority != undefined && (moment.unix() >= (x.timerange[0] - 86400) && moment.unix() < x.timerange[1]));
+            const withprio = data.filter(x => x.priority != undefined && (moment().unix() >= (x.timerange[0] - 86400) && moment().unix() < x.timerange[1]));
             const nonprio = data.filter(x => x.priority == undefined);
             for (let i = 0; i< withprio.length; i++) {
                 tempd.push(withprio[i])

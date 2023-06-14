cac0afee84fcd447775ddc9968688266229b291a
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2023-06-14 15:35:47 +0700

    Emergency update

--- src/component/official.js
+++ src/component/official.js
@@ -58,7 +58,7 @@ const Offi = ({fet, setSec, width}) => {
              <div className={"stage justify-content-center pt-5" + (width > 600 ? ' pl-5 pr-5' : ' pl-3 pr-3')}>
              <br />
              <div className='row'>
-             {Arr.length > 0 ? Arr.map((item,i) => (
+             {Arr.length > 0 ? Arr.map((item,i) => item.link.include('cgm48offcial') && (
                  <div className={"col-md-12 mb-5" + (width > 600 ? ' pl-5 pr-5' : '')} data-aos="zoom-in-down">
                  <Card onClick={() => hand(true,item)}>
                 <CardContent>

01108a63a36fa092ad7a633dc03835029b864cb2
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-02-23 12:39:24 +0700

    Quick Update: [2024-02-23 12:39:19.820]

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const bnklink = "https://cdn.statically.io/gl/cpx2017/iamprofile@main/sing15profile/";
+export const bnklink = "https://cdn.statically.io/gl/cpx2017/iamprofile@main/bnk16th/bnk/";
 export const bnktype = ".jpg";
 export const cgmlink = "https://cdn.statically.io/gl/cpx2017/iamprofile@main/cgm2ndalbum/";
 export const cgmtype = ".jpg";

2916886cbfb07b8c7ccd162291200f7a11c1c393
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-04-02 11:20:27 +0700

    Quick Update: [2024-04-02 11:20:22.804]

--- src/component/mssongkran.js
+++ src/component/mssongkran.js
@@ -63,17 +63,17 @@ const fetchapi = () => {
         .then(response => response.json())
         .then(data => {
             setData(data)
-            fetch(fet + '/cgm48/tkxlist/0xa8E49Dce482E68df7D89509b4810dc466bd6f24A?chartLimit=6', {
-                method :'post'
-            })
-                .then(response => response.json())
-                .then(datax => {
-                    setData2(datax)
-                    setTimeout(() => {
-                        setFet(true)
-                    }, 10000);
-                }); 
         }); 
+        fetch(fet + '/cgm48/tkxlist/0xa8E49Dce482E68df7D89509b4810dc466bd6f24A?chartLimit=6', {
+          method :'post'
+      })
+          .then(response => response.json())
+          .then(datax => {
+              setData2(datax)
+          }); 
+          setTimeout(() => {
+              setFet(true)
+          }, 30000);
 }
 
     React.useEffect(() => {

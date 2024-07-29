e64ea5357ff2932a0d6c1ea7d0b97349228ab7cc
Chinnathorn P <chinnathorn.p@gmail.com>
2024-07-29 14:48:14 +0700

    Quick Update: [2024-07-29 14:48:06.826]

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const bnklink = "https://cdn.statically.io/gl/cpx2017/iamprofile@main/bnk16th/bnk/";
+export const bnklink = "https://cdn.statically.io/gl/cpx2017/iamprofile@main/bnk17thsing/";
 export const bnktype = ".jpg";
 export const cgmlink = "https://cdn.statically.io/gl/cpx2017/iamprofile@main/cgm7th/";
 export const cgmtype = ".jpg";

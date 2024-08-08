aa64683ccf1a7d1dc61e448e40a19002856262de
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-08-08 09:25:11 +0700

    Quick Update: [2024-08-08 09:25:06.905]

--- src/component/apisupport.js
+++ src/component/apisupport.js
@@ -24,6 +24,7 @@ const API = ({setSec, width}) => {
                 <CardHeader title='API Service' subheader='Unofficial CGM48 Members Public API' />
                 <hr />
                 <p>We also serve CGM48 members profile on Public API service. Member profile contents which you see in website are using this service also. Please see our <a href='https://cp-apicenter.pages.dev/cgm48' target='_blank'>documentation</a> here</p>
+                <h6 className='text-info'>CPXDev Data Connect has been discontinued in October 31, 2024 in 11:59 PM. (UTC +07:00). You can request access token until August 31, 2024 in 11:59 PM. (UTC +07:00). Including BNK48 and CGM48 Fan Space Public API will be closed too.</h6>
                 <hr />
                 <CardActionArea className={stat === true ? 'text-success' : stat === false ? 'text-danger' : ''}>
                     API Service Status: {stat === true ? 'Systems are great.' : stat === false ? 'Systems is temporary down or under maintenance.' : 'Checking API status'}

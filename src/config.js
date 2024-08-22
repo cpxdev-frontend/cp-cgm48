3eb9689d1170956e1c0d9e869115b063de215584
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-08-22 11:38:23 +0700

    Quick Update: [2024-08-22 11:38:18.864]

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const bnklink = "https://d2m23ocr3g32v7.cloudfront.net/cpxdevcdn/bnk48/";
+export const bnklink = "https://d2m23ocr3g32v7.cloudfront.net/bnk48/";
 export const bnktype = ".jpg";
-export const cgmlink = "https://d2m23ocr3g32v7.cloudfront.net/cpxdevcdn/cgm48/";
+export const cgmlink = "https://d2m23ocr3g32v7.cloudfront.net/cgm48/";
 export const cgmtype = ".jpg";
